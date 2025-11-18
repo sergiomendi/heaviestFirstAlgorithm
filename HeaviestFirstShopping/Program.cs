7a8e46ccaeeaeabd2e91eb08eeec74ac36a93b8e
Sergio Mendiola Arraez <118443598+sergiomendi@users.noreply.github.com>
2025-11-18 20:49:46 +0100

    First basic version

---
+++ HeaviestFirstShopping/Program.cs
@@ -0,0 +1,66 @@
+﻿
+namespace Supermarket
+{
+    public class Basket
+    {
+        private double maxWeight;
+        private double currentWeight = 0.0;
+        private List<Product> products;
+        public IReadOnlyList<Product> Products => products;
+
+        public Basket(double maxWeight)
+        {
+            this.maxWeight = maxWeight;
+            products = new List<Product>();
+        }
+
+        public void AddProducts(List<Product> productsToAdd)
+        {
+            productsToAdd.Sort((a, b) => b.Weight.CompareTo(a.Weight));
+            foreach (var product in productsToAdd)
+            {
+                if (currentWeight + product.Weight <= maxWeight)
+                {
+                    products.Add(product);
+                    currentWeight += product.Weight;
+                }
+            }
+        }
+    }
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+
+        public Product(string name, double weight)
+        {
+            Name = name;
+            Weight = weight;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<Product> itemsIWantToBuy = new List<Product>
+            {
+                new Product("Rice", 4.0),
+                new Product("Potatoes", 3.0),
+                new Product("Whole chicken", 5.0),
+                new Product("Sliced bread", 12.0),
+            };
+            Basket basket = new Basket(20.0);
+
+            basket.AddProducts(itemsIWantToBuy); // Rice must not be added
+
+            Console.WriteLine("Items in basket:");
+            foreach (var product in basket.Products)
+            {
+                Console.WriteLine(product.Name + ": " + product.Weight + " kg");
+            }
+        }
+    }
+
+}
